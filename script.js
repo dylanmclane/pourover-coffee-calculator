ff59dcbea35c1e7c69f472f85f6f8919e383b40b
Dylan McLane <45742229+dylanmclane@users.noreply.github.com>
2023-04-04 23:33:45 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
-// Intro message/default page
-// let choices = Bold, Medium, Light
-console.log('Select a desired strength: ')
-// Offer Bold, Medium, Light
-// Or explain ratios?
+const form = document.querySelector('form');
+const result = document.getElementById('result');
 
-/*
-const bold = 
-const med = 
-const light =
-*/
-
-let strength = 'bold' 
-// placeholder value, string in future
-console.log('You have selected a ' + strength + ' coffee.')
-
-let coffee = 20;
-let water = 300;
-let ratio = water/coffee;
-
-// Store user choices and final explanation
-const message = 'A good ratio for a ' + strength + ' pourover coffee is 1:' + ratio
-
-const explanation = '. This means you should grind ' + coffee +' grams of coffee beans and pour ' + water + ' grams of water over your ground beans'
-
-// Find out if it is better to store text in the JS or in the HTML. Not using React or Angular.
-
-// final output
-console.log(message + explanation)
+form.addEventListener('submit', function(event) {
+	event.preventDefault();
+	const coffeeStrength = document.getElementById('coffee-strength').value;
+	const coffeeAmount = Number(document.getElementById('coffee-amount').value);
+	const coffeeRatio = {
+		weak: 1 / 15,
+		medium: 1 / 16,
+		strong: 1 / 17
+	}[coffeeStrength];
+	const waterAmount = coffeeAmount / coffeeRatio;
+	
+	result.textContent = `To make ${coffeeAmount} grams of coffee with a desired coffee strength of ${coffeeStrength}, use a coffee-to-water ratio of 1:${(1/coffeeRatio).toFixed(1)}. Use ${waterAmount.toFixed(1)} grams of water.`;
+});
